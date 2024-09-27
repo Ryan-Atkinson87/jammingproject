ba2bafc2424950633b6dddea3375e18bf5cea76c
ryan.atkinson <atkinson.ryan87@gmail.com>
2024-09-27 14:59:33 +0100

    created files and basic file structure

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Start of my spotify project on codecademy!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
